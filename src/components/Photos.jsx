361a1f25a28c39889371bb6f20ceeb8673d0c887
Marvin-kamwenji <marvinkamwenjih@gmail.com>
2022-02-23 12:32:00 +0300

    Adding uploading and removing features

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
 import Gallery from "react-photo-gallery";
+import ImageUploading from 'react-images-uploading';
 
 const photo = [
     {
@@ -59,6 +60,8 @@ const photo = [
     }
 ];
 
+
+
 const Content = {
     height: "700px",
     display: "inline-flex",
@@ -71,12 +74,65 @@ const Content = {
 };
 
 function Photos(){
+
+
+    const [images, setImages] = useState([]);
+const maxNumber = 69;
+
+const onChange = (imageList, addUpdateIndex) => {
+    console.log(imageList, addUpdateIndex);
+    setImages(imageList);
+};
+
+
     return(
+    <div>
+        <ImageUploading
+           multiple
+           value={images}
+           onChange={onChange}
+           maxNumber={maxNumber}
+           dataURLKey="data_url">
+
+               {
+                   ({
+                           imageList,
+                           onImageUpload,
+                           onImageRemoveAll,
+                           onImageUpdate,
+                           onImageRemove,
+                           isDragging,
+                           dragProps,
+                    }) => (
+                        <div className="upload_image-wrapper">
+                            <button
+                               style={isDragging ? { color: 'red' } : undefined}
+                               onClick={onImageUpload}
+                               {...dragProps} >
+                                  Click or Drop here
+                            </button>
+
+                            <button onClick={onImageRemoveAll}>Remove all images</button>
+                                {imageList.map((image, index) => (
+                                    <div key={index} className="image-item">
+                                       <img src={image.data_url} alt="" width="100" />
+                                           <div className="image-item__btn-wrapper">
+                                              <button onClick={() => onImageUpdate(index)}>Update</button>
+                                              <button onClick={() => onImageRemove(index)}>Remove</button>
+                                          </div>
+                                      </div>
+                                ))}      
+                        </div>
+                    )
+               }
+
+        </ImageUploading>
         <div className="flex space-x-3 overflow-y-scroll scrollbar-hide p-3 -ml-3">
              <Gallery direction={"row"} margin={40} photos={photo} />
              ))
         </div>
 
+    </div>  
     );
 }
 
